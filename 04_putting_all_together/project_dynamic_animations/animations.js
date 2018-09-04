d185ef0369f0495b6c6047de1f3afd93cc59c043
PetePhx <pete0phx@yandex.com>
2018-09-04 12:25:22 -0700

    add animations

--- 04_putting_all_together/project_dynamic_animations/animations.js
+++ 04_putting_all_together/project_dynamic_animations/animations.js
@@ -13,18 +13,43 @@ $(function () {
   function createElement(data) {
     var $div = $('<div></div>', {
       "class": data.shape_type,
-      css: {
-            left: data.start_x + 'px',
-            top: data.start_y + 'px',
-          },
       data: data,
     });
+    resetElement($div);
     return $div;
+  }
+
+  function animateElement() {
+    var $e = $(this);
+    var data = $e.data();
+    resetElement($e);
+    $e.animate({
+      left: data.end_x + 'px',
+      top: data.end_y + 'px',
+    }, Number(data.duration));
+  }
+
+  function resetElement($e) {
+    var data = $e.data();
+    $e.css({
+      left: data.start_x + 'px',
+      top: data.start_y + 'px',
+    });
   }
 
   $form.on('submit', function (e) {
     e.preventDefault();
     var data = getFormObject($(this));
     $canvas.append(createElement(data));
+  });
+
+  $('#animate').on('click', function (e) {
+    e.preventDefault();
+    $canvas.find('div').each(animateElement);
+  });
+
+  $('#stop').on('click', function (e) {
+    e.preventDefault();
+    $canvas.find('div').stop();
   });
 });
