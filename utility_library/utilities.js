80c97bc735eee04ab4216d20abcfd2bc30788961
PetePhx <pete0phx@yandex.com>
2018-09-05 12:45:19 -0700

    utilities part III

--- utility_library/utilities.js
+++ utility_library/utilities.js
@@ -1,26 +1,26 @@
 (function () {
-  var _ = function (arr) {
+  var _ = function (aggr) {
     var propsMatch = function (obj, elm) {
       for (var prop in obj) {
-        if (!elm.hasOwnProperty(prop) || obj[prop] !== elm[prop]) return false;
+        if (!(prop in elm) || obj[prop] !== elm[prop]) return false;
       }
       return true;
     };
 
     var u = {
-      first: function () { return arr[0]; },
-      last: function () { return arr[arr.length - 1]; },
+      first: function () { return aggr[0]; },
+      last: function () { return aggr[aggr.length - 1]; },
       without: function (...args) {
-        return arr.filter(function (elm) {
+        return aggr.filter(function (elm) {
           return !args.includes(elm);
         });
       },
       lastIndexOf: function (val) {
-        return arr.lastIndexOf(val);
+        return aggr.lastIndexOf(val);
       },
       sample: function (num) {
         sampleArr = [];
-        copyArr = arr.slice();
+        copyArr = aggr.slice();
         get = function () {
           var idx = Math.floor(Math.random() * copyArr.length);
           var elm = copyArr[idx];
@@ -32,22 +32,35 @@
         return sampleArr;
       },
       findWhere: function (props) {
-        for (var i = 0; i < arr.length; i += 1) {
-          if (propsMatch(props, arr[i])) return arr[i];
+        for (var i = 0; i < aggr.length; i += 1) {
+          if (propsMatch(props, aggr[i])) return aggr[i];
         }
         return undefined;
       },
       where: function (props) {
-        return arr.filter(function (elm) {
+        return aggr.filter(function (elm) {
           return propsMatch(props, elm);
         });
       },
       pluck: function (prop) {
-        return arr.filter(function (elm) { return elm.hasOwnProperty(prop); })
+        return aggr.filter(function (elm) { return (prop in elm); })
                   .map(function (elm) { return elm[prop]; });
       },
-      keys: function () { return Object.keys(arr); },
-      values: function () { return Object.values(arr); },
+      keys: function () { return Object.keys(aggr); },
+      values: function () { return Object.values(aggr); },
+      pick: function (...args) {
+        var newObj = {};
+        args.forEach(function (prop) { newObj[prop] = aggr[prop]; });
+        return newObj;
+      },
+      omit: function (...args) {
+        var newObj = {};
+        for (var prop in aggr) {
+          if (!args.includes(prop)) newObj[prop] = aggr[prop];
+        }
+        return newObj;
+      },
+      has: function (prop) { return (prop in aggr); },
     };
     return u;
   };
@@ -55,6 +68,12 @@
   _.range = function (a, b) {
     if (b === undefined) [a, b] = [0, a];
     return Array(b - a).fill().map(function (elm, idx) { return idx + a; });
+  };
+
+  _.extend = function (obj1, obj2, ...args) {
+      var tempObj = (args.length === 0) ? obj2 : _.extend(obj2, ...args);
+      for (var prop in tempObj) { obj1[prop] = tempObj[prop]; }
+      return obj1;
   };
 
   window._ = _;
