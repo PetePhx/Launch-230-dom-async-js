df895b2e90fa62f9377e25e5f8fad46bb4649eb8
PetePhx <pete0phx@yandex.com>
2018-09-17 13:32:24 -0700

    gallery photo comments

--- 05_dynamic_content/photo_gallery/public/javascripts/gallery.js
+++ 05_dynamic_content/photo_gallery/public/javascripts/gallery.js
@@ -1,17 +1,44 @@
 $(function () {
   var templates = {};
   var photos;
+  var photo_id;
 
   $('script[type="text/x-handlebars"]').each(function () {
     templates[$(this).attr('id')] = Handlebars.compile($(this).html());
+  });
+
+  $('[data-type=partial]').each(function () {
+    Handlebars.registerPartial($(this).attr("id"), $(this).html());
+
   });
 
   $.ajax({
     url: "/photos",
     success: function (json) {
       photos = json;
-      $('#slides').html(templates.photos({photos: photos}));
-      $('section > header').html(templates.photo_information(photos[0]));
+      renderPhotos();
+      renderPhotoInformation(0);
+      getCommentsFor(photos[0].id);
+      // photo_id = $('section a').attr('data-id');
     },
   });
+
+  function renderPhotos() {
+    $('#slides').html(templates.photos({photos: photos}));
+  }
+
+  function renderPhotoInformation(idx) {
+    $('section > header').html(templates.photo_information(photos[idx]));
+  }
+
+  function getCommentsFor(idx) {
+    $.ajax({
+      url: '/comments',
+      data: 'photo_id=' + idx,
+      success: function (comment_json) {
+        console.dir(comment_json);
+        $('#comments ul').html(templates.comments({comments: comment_json}));
+      },
+    });
+  }
 });
