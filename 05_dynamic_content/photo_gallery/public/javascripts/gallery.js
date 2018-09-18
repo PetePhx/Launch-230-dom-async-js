a5998240120ea07ae50e5c45c09f0cf6f4bfcd5b
PetePhx <pete0phx@yandex.com>
2018-09-18 11:45:40 -0700

    gallery prev/next functionality

--- 05_dynamic_content/photo_gallery/public/javascripts/gallery.js
+++ 05_dynamic_content/photo_gallery/public/javascripts/gallery.js
@@ -9,17 +9,52 @@ $(function () {
 
   $('[data-type=partial]').each(function () {
     Handlebars.registerPartial($(this).attr("id"), $(this).html());
-
   });
+
+  var slideshow = {
+    init: function () {
+      this.bind();
+    },
+    bind: function () {
+      this.$el.find('a.prev').on('click', this.prevSlide.bind(this));
+      this.$el.find('a.next').on('click', this.nextSlide.bind(this));
+    },
+    $el: $('#slideshow'),
+    prevSlide: function (e) {
+      e.preventDefault();
+      var $current = this.$el.find("figure:visible");
+      var $prev = $current.prev("figure");
+
+      if ($prev.length === 0) $prev = this.$el.find("figure").last();
+      $current.fadeOut(this.fadeDur);
+      $prev.fadeIn(this.fadeDur);
+      this.renderPhotoContent($prev.attr("data-id"));
+    },
+    nextSlide: function (e) {
+      e.preventDefault();
+      var $current = this.$el.find("figure:visible");
+      var $next = $current.next("figure");
+
+      if ($next.length === 0) $next = this.$el.find("figure").first();
+      $current.fadeOut(this.fadeDur);
+      $next.fadeIn(this.fadeDur);
+      this.renderPhotoContent($next.attr("data-id"));
+    },
+    fadeDur: 500, //miliseconds
+    renderPhotoContent: function (idx) {
+      renderPhotoInformation(idx);
+      getCommentsFor(idx);
+    },
+  };
 
   $.ajax({
     url: "/photos",
     success: function (json) {
       photos = json;
       renderPhotos();
-      renderPhotoInformation(0);
+      renderPhotoInformation(1);
+      slideshow.init();
       getCommentsFor(photos[0].id);
-      // photo_id = $('section a').attr('data-id');
     },
   });
 
@@ -28,7 +63,8 @@ $(function () {
   }
 
   function renderPhotoInformation(idx) {
-    $('section > header').html(templates.photo_information(photos[idx]));
+    var photo = photos.find(function (elm) { return elm.id === Number(idx); });
+    $('section > header').html(templates.photo_information(photo));
   }
 
   function getCommentsFor(idx) {
@@ -36,7 +72,6 @@ $(function () {
       url: '/comments',
       data: 'photo_id=' + idx,
       success: function (comment_json) {
-        console.dir(comment_json);
         $('#comments ul').html(templates.comments({comments: comment_json}));
       },
     });
