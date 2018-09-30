20052311492beb96372bcae2ba079ef356298c8e
PetePhx <pete0phx@yandex.com>
2018-09-29 18:25:32 -0700

    autocomplete: 2: set up UI

---
+++ autocomplete/public/javascripts/autocomplete.js
@@ -0,0 +1,37 @@
+var Autocomplete = {
+  wrapInput: function() {
+    var wrapper = document.createElement('div');
+    wrapper.classList.add('autocomplete-wrapper');
+    this.input.parentNode.appendChild(wrapper);
+    wrapper.appendChild(this.input);
+  },
+
+  createUI: function() {
+    var listUI = document.createElement('ul');
+    listUI.classList.add('autocomplete-ui');
+    this.input.parentNode.appendChild(listUI);
+    this.listUI = listUI;
+
+    var overlay = document.createElement('div');
+    overlay.classList.add('autocomplete-overlay');
+    overlay.style.width = this.input.clientWidth + 'px';
+
+    this.input.parentNode.appendChild(overlay);
+    this.overlay = overlay;
+  },
+
+  init: function() {
+    this.input = document.querySelector('input');
+    this.url = '/countries?matching=';
+
+    this.listUI = null;
+    this.overlay = null;
+
+    this.wrapInput();
+    this.createUI();
+  }
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  Autocomplete.init();
+});
